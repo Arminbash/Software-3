d72dbbb18d1903be213c842b6a99a8a7ae582b70
Johnny Arcia <cosmicvanbuuren@hotmail.com>
2019-03-24 22:23:44 -0600

    4

--- Cobit 5/Cobit 5/frmRellenarEmpresa.Designer.cs
+++ Cobit 5/Cobit 5/frmRellenarEmpresa.Designer.cs
@@ -31,6 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRellenarEmpresa));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombreEmpresa = new System.Windows.Forms.TextBox();
+            this.txtNombreUsuario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSig = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -53,11 +59,76 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Datos del usuario";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(218, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre de la empresa";
+            // 
+            // txtNombreEmpresa
+            // 
+            this.txtNombreEmpresa.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreEmpresa.Location = new System.Drawing.Point(273, 78);
+            this.txtNombreEmpresa.Name = "txtNombreEmpresa";
+            this.txtNombreEmpresa.Size = new System.Drawing.Size(327, 22);
+            this.txtNombreEmpresa.TabIndex = 3;
+            // 
+            // txtNombreUsuario
+            // 
+            this.txtNombreUsuario.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreUsuario.Location = new System.Drawing.Point(273, 196);
+            this.txtNombreUsuario.Name = "txtNombreUsuario";
+            this.txtNombreUsuario.Size = new System.Drawing.Size(327, 22);
+            this.txtNombreUsuario.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre del usuario";
+            // 
+            // btnSig
+            // 
+            this.btnSig.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSig.Location = new System.Drawing.Point(725, 347);
+            this.btnSig.Name = "btnSig";
+            this.btnSig.Size = new System.Drawing.Size(139, 50);
+            this.btnSig.TabIndex = 8;
+            this.btnSig.Text = "SIGUIENTE";
+            this.btnSig.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 50);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Atras";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // frmRellenarEmpresa
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(876, 592);
+            this.ClientSize = new System.Drawing.Size(876, 409);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSig);
+            this.Controls.Add(this.txtNombreUsuario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNombreEmpresa);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -76,5 +147,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombreEmpresa;
+        private System.Windows.Forms.TextBox txtNombreUsuario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSig;
+        private System.Windows.Forms.Button button1;
     }
 }
